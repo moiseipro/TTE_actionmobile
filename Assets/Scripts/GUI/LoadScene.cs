35ee454ae1a05c5c1e72b34c4f173d692ddaa710
makey <sasha.makeeff@gmail.com>
2019-01-13 00:00:52 +0300

    Add scene for loading

---
+++ Assets/Scripts/GUI/LoadScene.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+
+    public int sceneID;
+
+    public Text progressText;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(AsyncLoad());
+    }
+	
+    IEnumerator AsyncLoad()
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneID);
+        while (!operation.isDone)
+        {
+            float progress = operation.progress / 0.9f;
+            progressText.text = string.Format("{0:0}%", progress*100f);
+            yield return null;
+        }
+    }
+
+}
