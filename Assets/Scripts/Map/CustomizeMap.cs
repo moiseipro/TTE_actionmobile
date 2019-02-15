8515be861a616f5b525ea1afb9d3b56981a3b082
makey <sasha.makeeff@gmail.com>
2019-02-15 17:21:21 +0300

    Delete postprosses2 asset

--- Assets/Scripts/Map/CustomizeMap.cs
+++ Assets/Scripts/Map/CustomizeMap.cs
@@ -37,10 +37,41 @@ public class CustomizeMap : MonoBehaviour {
     // Use this for initialization
     void Start () {
         gameManager = GameObject.FindWithTag("Manager").GetComponent<SceneController>();
-        foreach (Vector3 vec in spawnStaticPoints)
+
+        if (spawnStaticPoints != null)
         {
-            GameObject obj = Instantiate(gameManager.objectPrefab[Random.Range(0, gameManager.objectPrefab.Length)], transform.TransformPoint(vec/50) + new Vector3(0, 5f, 0f), Quaternion.identity);
-            obj.transform.rotation = Quaternion.AngleAxis(Random.Range(0,360), Vector3.up);
+            foreach (Vector3 vec in spawnStaticPoints)
+            {
+                GameObject obj = Instantiate(gameManager.objectPrefab[Random.Range(0, gameManager.objectPrefab.Length)], transform.TransformPoint(vec / 50) + new Vector3(0, 5f, 0f), Quaternion.identity);
+                obj.transform.rotation = Quaternion.AngleAxis(Random.Range(0, 360), Vector3.up);
+            }
+        }
+
+        if (spawnDestrPoints != null)
+        {
+            foreach (Vector3 vec in spawnDestrPoints)
+            {
+                GameObject obj = Instantiate(gameManager.objectInterPrefab[Random.Range(0, gameManager.objectInterPrefab.Length)], transform.TransformPoint(vec / 50) + new Vector3(0, 5f, 0f), Quaternion.identity);
+                obj.transform.rotation = Quaternion.AngleAxis(Random.Range(0, 360), Vector3.up);
+            }
+        }
+
+        if (spawnTrapPoints != null)
+        {
+            foreach (Vector3 vec in spawnTrapPoints)
+            {
+                GameObject obj = Instantiate(gameManager.objectTrapPrefab[Random.Range(0, gameManager.objectTrapPrefab.Length)], transform.TransformPoint(vec / 50) + new Vector3(0, 5f, 0f), Quaternion.identity);
+                obj.transform.rotation = Quaternion.AngleAxis(Random.Range(0, 360), Vector3.up);
+            }
+        }
+
+        if (spawnChestPoints != null)
+        {
+            foreach (Vector3 vec in spawnChestPoints)
+            {
+                GameObject obj = Instantiate(gameManager.objectChestPrefab[Random.Range(0, gameManager.objectChestPrefab.Length)], transform.TransformPoint(vec / 50) + new Vector3(0, 5f, 0f), Quaternion.identity);
+                obj.transform.rotation = Quaternion.AngleAxis(Random.Range(0, 360), Vector3.up);
+            }
         }
     }
 
@@ -50,6 +81,33 @@ public class CustomizeMap : MonoBehaviour {
         if (spawnStaticPoints != null)
         {
             foreach (Vector3 vec in spawnStaticPoints)
+            {
+                Gizmos.DrawSphere(transform.TransformPoint(vec / 50) + new Vector3(0, 5f, 0f), 1);
+            }
+        }
+
+        Gizmos.color = destrColor;
+        if (spawnDestrPoints != null)
+        {
+            foreach (Vector3 vec in spawnDestrPoints)
+            {
+                Gizmos.DrawSphere(transform.TransformPoint(vec / 50) + new Vector3(0, 5f, 0f), 1);
+            }
+        }
+
+        Gizmos.color = trapColor;
+        if (spawnTrapPoints != null)
+        {
+            foreach (Vector3 vec in spawnTrapPoints)
+            {
+                Gizmos.DrawSphere(transform.TransformPoint(vec / 50) + new Vector3(0, 5f, 0f), 1);
+            }
+        }
+
+        Gizmos.color = chestColor;
+        if (spawnChestPoints != null)
+        {
+            foreach (Vector3 vec in spawnChestPoints)
             {
                 Gizmos.DrawSphere(transform.TransformPoint(vec / 50) + new Vector3(0, 5f, 0f), 1);
             }
