840e6e95f434353e898456228077b1595aa9f7dd
makey <sasha.makeeff@gmail.com>
2019-02-25 23:21:15 +0300

    Add pause

--- Assets/Scripts/Charackter/PlayerManager.cs
+++ Assets/Scripts/Charackter/PlayerManager.cs
@@ -7,8 +7,12 @@ using UnityEngine.UI;
 public class PlayerManager : MonoBehaviour {
 
     private GameObject player;
+    private Text key, nugget;
     private int moneyValue, keyValue,
         playerLevel;
+    private bool isPause = false;
+    private RectTransform artifactPanel, pause, statsPanel;
+    private float speedUI = 150f;
 
     [HideInInspector]
     public int levelGame = 0;
@@ -17,18 +21,61 @@ public class PlayerManager : MonoBehaviour {
     void Start () {
         levelGame = PlayerPrefs.GetInt("Level");
         player = GameObject.FindWithTag("Player");
+        key = GameObject.Find("KeyPlate").GetComponentInChildren<Text>();
+        nugget = GameObject.Find("NuggetPlate").GetComponentInChildren<Text>();
+        nugget.text = moneyValue.ToString();
+        key.text = keyValue.ToString();
+        artifactPanel = GameObject.Find("ArtifactPanel").GetComponent<RectTransform>();
+        pause = GameObject.Find("Pause").GetComponent<RectTransform>();
+        statsPanel = GameObject.Find("StatsPanel").GetComponent<RectTransform>();
     }
 	
     public void AddMoney(int val)
     {
         moneyValue += val;
+        nugget.text = moneyValue.ToString();
     }
 
     public void AddKey()
     {
-        moneyValue++;
+        keyValue++;
+        key.text = keyValue.ToString();
     }
 
+
+    private void FixedUpdate()
+    {
+        if (isPause == true)
+        {
+            artifactPanel.anchoredPosition = Vector2.MoveTowards(artifactPanel.anchoredPosition, new Vector2(-225, 0), speedUI);
+            pause.anchoredPosition = Vector2.MoveTowards(pause.anchoredPosition, new Vector2(645, -645), speedUI);
+            statsPanel.anchoredPosition = Vector2.MoveTowards(statsPanel.anchoredPosition, new Vector2(225, -115), speedUI);
+        } else if(isPause == false)
+        {
+            artifactPanel.anchoredPosition = Vector2.MoveTowards(artifactPanel.anchoredPosition, new Vector2(225, 0), speedUI);
+            pause.anchoredPosition = Vector2.MoveTowards(pause.anchoredPosition, new Vector2(720, -75), speedUI);
+            statsPanel.anchoredPosition = Vector2.MoveTowards(statsPanel.anchoredPosition, new Vector2(-225, -115), speedUI);
+        }
+    }
+
+    public void Pause()
+    {
+        if (isPause == false)
+        {
+            isPause = true;
+            StartCoroutine(StartPause());
+        } else if(isPause == true)
+        {
+            isPause = false;
+            Time.timeScale = 1;
+        }
+    }
+
+    IEnumerator StartPause()
+    {
+        yield return new WaitForSeconds(0.3f);
+        Time.timeScale = 0;
+    }
 
     public void ReloadLevel()
     {
