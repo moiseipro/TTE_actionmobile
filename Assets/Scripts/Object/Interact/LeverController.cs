c2f7bcbd6d59bcd0a195d28d50696dbfbf2c78fd
makey <sasha.makeeff@gmail.com>
2019-03-12 21:51:57 +0300

    Added lowers the bridge

---
+++ Assets/Scripts/Object/Interact/LeverController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverController : MonoBehaviour {
+
+    public Animator activatebleObject;
+    private Animator thisAnim;
+
+	// Use this for initialization
+	void Start () {
+        thisAnim = GetComponent<Animator>();
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            thisAnim.SetTrigger("Activate");
+            activatebleObject.SetTrigger("Activate");
+        }
+    }
+
+    public void TakeDamage()
+    {
+        gameObject.tag = "Map";
+        thisAnim.SetTrigger("Activate");
+        activatebleObject.SetTrigger("Activate");
+    }
+}
